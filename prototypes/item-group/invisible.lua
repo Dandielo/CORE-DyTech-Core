8075c3fd724bb9833e9c388722cc97d48884ada6
Dandielo <daniel.penkala@gmail.com>
2016-12-25 11:56:10 +0100

    Reindenting invisible.lua

--- prototypes/item-group/invisible.lua
+++ prototypes/item-group/invisible.lua
@@ -1,29 +1,28 @@
-data:extend(
+data:extend
 {
-  {
-    type = "item-group",
-    name = "dytech-invisible",
-    order = "z-dytech-invisible",
-    inventory_order = "zzzzzz-dytech-invisible",
-    icon = "__CORE-DyTech-Core__/graphics/item-group/danger.png",
-  },
-  {
-    type = "item-subgroup",
-    name = "checker-items",
-    group = "dytech-invisible",
-    order = "1"
-  },
-  {
-    type = "item-subgroup",
-    name = "ruins",
-    group = "dytech-invisible",
-    order = "2"
-  },
-  {
-    type = "item-subgroup",
-    name = "modular-tools",
-    group = "dytech-invisible",
-    order = "3"
-  },
+    {
+        type = "item-group",
+        name = "dytech-invisible",
+        order = "z-dytech-invisible",
+        inventory_order = "zzzzzz-dytech-invisible",
+        icon = "__CORE-DyTech-Core__/graphics/item-group/danger.png",
+    },
+    {
+        type = "item-subgroup",
+        name = "checker-items",
+        group = "dytech-invisible",
+        order = "1"
+    },
+    {
+        type = "item-subgroup",
+        name = "ruins",
+        group = "dytech-invisible",
+        order = "2"
+    },
+    {
+        type = "item-subgroup",
+        name = "modular-tools",
+        group = "dytech-invisible",
+        order = "3"
+    },
 }
-)
