2a6680a90d2c58afbd7614f552ec86263165ed84
dandielo <daniel.penkala@gmail.com>
2016-12-28 23:42:48 +0100

    Reindenting rubber-tree related files

--- prototypes/trees/rubber-tree-recipe.lua
+++ prototypes/trees/rubber-tree-recipe.lua
@@ -1,15 +1,16 @@
-data:extend(
+data:extend
 { 
-  {
-    type = "recipe",
-    name = "rubber-seed",
-	icon = "__CORE-DyTech-Core__/graphics/rubber-tree/stage-1.png",
-    energy_required = 1,
-    ingredients = 
-	{
-	  {"resin", 1}
-	},
-    result = "rubber-seed"
-  },
+    {
+        type = "recipe",
+        name = "rubber-seed",
+        icon = "__CORE-DyTech-Core__/graphics/rubber-tree/stage-1.png",
+
+        result = "rubber-seed",
+        energy_required = 1,
+
+        ingredients = 
+        {
+            { "resin", 1 } -- rubber seed from resin?
+        },
+    },
 }
-)
