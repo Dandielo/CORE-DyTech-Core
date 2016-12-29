ca748db2d205dc0145faaa1f6a063ffbed4de58b
dandielo <daniel.penkala@gmail.com>
2016-12-29 19:25:29 +0100

    Reindenting autoplace-controls.lua

--- prototypes/autoplace-controls.lua
+++ prototypes/autoplace-controls.lua
@@ -1,10 +1,9 @@
-data:extend(
+data:extend
 {
-  {
-    type = "autoplace-control",
-    name = "gemstones",
-    richness = true,
-    order = "g-s"
-  },
+    {
+        type = "autoplace-control",
+        name = "gemstones",
+        richness = true,
+        order = "g-s"
+    },
 }
-)
