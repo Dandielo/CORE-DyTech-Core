9cb6358c3da7ad6d346ce7a4429a9488de3c1ab7
dandielo <daniel.penkala@gmail.com>
2016-12-31 13:33:40 +0100

    Removed the compressor child folder and created a dedicated file

--- prototypes/machines/sawmill.lua
+++ prototypes/machines/sawmill.lua
@@ -1,6 +1,36 @@
 require ("prototypes.machines.functions")
 require "prototypes.internal-config"
 
+dytech:extend 
+{
+    {
+        templates = { "machine", "machine-mk3", "machine-big", "sawmill" },
+
+        type = "assembling-machine",
+        name = "sawmill", 
+
+        -- Custom health and resistances
+        max_health = Health.Tier2,
+        resistances = Resistances.Tier2,
+
+        -- Energy usage
+        energy_usage = "125kW",
+        energy_source =
+        {
+            type = "electric",
+            usage_priority = "secondary-input",
+            emissions = 0.01 / 0.5
+        },
+
+        -- Interactions
+        minable = { 
+            result = "sawmill"
+        },
+
+        -- Crafting
+        crafting_categories = { "sawmill" },
+    }
+}
 
 data:extend(
 {
