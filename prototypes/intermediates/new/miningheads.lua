87625f1aed6575f88d61a1f18194eabbaba1f327
dandielo <daniel.penkala@gmail.com>
2017-01-03 12:56:21 +0100

    Moved mining heads to a separate file,

--- prototypes/intermediates/new/miningheads.lua
+++ prototypes/intermediates/new/miningheads.lua
@@ -6,13 +6,10 @@ dytech:intermediate
 
         -- Prototype templates (each template create a prototype)
         templates =
-        {
-            {
-                template = "item",
-            },
+        { 
+            { "item" },
             {
                 template = "recipe",
-                enabled = false,
                 ingredients = 
                 { 
                     { "steel-plate", 2 }
@@ -23,5 +20,120 @@ dytech:intermediate
                 name = "steel-processing"
             }
         }
-    }
+    },
+    -- { -- DyTech - Machine
+    --     name = "mininghead-1",
+    --     subgroup = "mining",
+
+    --     -- Prototype templates (each template create a prototype)
+    --     templates =
+    --     {
+    --         { "item" },
+    --         {
+    --             template = "recipe",
+    --             energy_required = 5,
+    --             ingredients = 
+    --             { 
+    --                 { "mininghead-0", 1 },
+    --                 { "raw-ruby", 2 }
+    --             },
+    --         },
+    --         { 
+    --             template = "tech-add",
+    --             name = "miner-2"
+    --         }
+    --     }
+    -- },
+    -- {
+    --     name = "mininghead-2",
+    --     subgroup = "mining",
+
+    --     -- Prototype templates (each template create a prototype)
+    --     templates =
+    --     {
+    --         { "item" },
+    --         {
+    --             template = "recipe",
+    --             energy_required = 10,
+    --             ingredients = 
+    --             { 
+    --                 { "mininghead-0", 1 },
+    --                 { "raw-sapphire", 2 }
+    --             },
+    --         },
+    --         { 
+    --             template = "tech-add",
+    --             name = "miner-3"
+    --         }
+    --     }
+    -- },
+    -- {
+    --     name = "mininghead-3",
+    --     subgroup = "mining",
+
+    --     -- Prototype templates (each template create a prototype)
+    --     templates =
+    --     {
+    --         { "item" },
+    --         {
+    --             template = "recipe",
+    --             energy_required = 15,
+    --             ingredients = 
+    --             { 
+    --                 { "mininghead-0", 1 },
+    --                 { "raw-emerald", 2 }
+    --             },
+    --         },
+    --         { 
+    --             template = "tech-add",
+    --             name = "miner-4"
+    --         }
+    --     }
+    -- },
+    -- {
+    --     name = "mininghead-4",
+    --     subgroup = "mining",
+
+    --     -- Prototype templates (each template create a prototype)
+    --     templates =
+    --     {
+    --         { "item" },
+    --         {
+    --             template = "recipe",
+    --             energy_required = 20,
+    --             ingredients = 
+    --             { 
+    --                 { "mininghead-0", 1 },
+    --                 { "raw-topaz", 2 }
+    --             },
+    --         },
+    --         { 
+    --             template = "tech-add",
+    --             name = "miner-5"
+    --         }
+    --     }
+    -- },
+    -- {
+    --     name = "mininghead-5",
+    --     subgroup = "mining",
+
+    --     -- Prototype templates (each template create a prototype)
+    --     templates =
+    --     {
+    --         { "item" },
+    --         {
+    --             template = "recipe",
+    --             energy_required = 25,
+    --             ingredients = 
+    --             { 
+    --                 { "mininghead-0", 1 },
+    --                 { "raw-diamond", 2 }
+    --             },
+    --         },
+    --         { 
+    --             template = "tech-add",
+    --             name = "miner-6"
+    --         }
+    --     }
+    -- }
 }
